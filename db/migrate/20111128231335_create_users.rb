08ed5bd30e7f7a85054f5f6ac3bc1aec2e2011d4
shanmukh49 <shanmukha49reddy@gmail.com>
2014-01-07 16:32:30 +0530

    first commit

---
+++ db/migrate/20111128231335_create_users.rb
@@ -0,0 +1,31 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :gender
+      t.text :address
+      t.integer :telephone
+      t.integer :mobile
+      t.string :course_enrolled
+      t.string :education
+      t.string :college_name
+      t.text :caddress
+      t.string :branch
+      t.string :course_intrested
+      t.boolean :contact_you
+      t.string :program_name
+      t.string :program_incharge
+      t.integer :program_hour
+      t.integer :program_week
+      t.date :start
+      t.date :end
+      t.integer :rfee
+      t.integer :cfee
+      t.integer :cmaterial
+      t.integer :total_cost
+
+      t.timestamps
+    end
+  end
+end
